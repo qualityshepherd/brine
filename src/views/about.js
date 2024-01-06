8b28940a72493da6be13555c58d15983d007f366
Brine <brian@qualityshepherd.com>
2024-01-06 08:19:41 -0700

    aboot

--- src/views/about.js
+++ src/views/about.js
@@ -3,7 +3,11 @@ const about = {
     return `
       <h2>About</h2>
 
-      Howdy. I'm brine. I'm a blabby nerd that lives in the desert. Contact me: <a href='&#109;a&#105;l&#116;o&#58;m%65%4&#48;b&#114;&#105;n%65&#46;%64e&#118;'>me&#64;brine&#46;d&#101;v</a>
+      <div class="center">
+        I'm a blabby nerd that lives in the desert.
+        <p>
+        Contact me: <a href='&#109;a&#105;l&#116;o&#58;m%65%4&#48;b&#114;&#105;n%65&#46;%64e&#118;'>me&#64;brine&#46;d&#101;v</a>
+      </div>
     `
   }
 }
