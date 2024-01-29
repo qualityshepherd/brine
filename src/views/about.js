cadaf1ec3cc2458b41667e357f67771acb9b2b9e
Brine <brian@qualityshepherd.com>
2024-01-29 12:46:36 -0700

    social

--- src/views/about.js
+++ src/views/about.js
@@ -5,8 +5,13 @@ const about = {
 
       <div class="center">
         I'm a blabby nerd that lives in the desert.
-        <p>
-        Contact me: <a href='&#109;a&#105;l&#116;o&#58;m%65%4&#48;b&#114;&#105;n%65&#46;%64e&#118;'>me&#64;brine&#46;d&#101;v</a>
+        <div class="social">
+          <a href='&#109;a&#105;l&#116;o&#58;m%65%4&#48;b&#114;&#105;n%65&#46;%64e&#118;' title="email"><img src="assets/images/social/email.png" alt="email"></a>
+          <a href="https://casadeocio.itch.io/" title="games on itch"><img src="assets/images/social/itch-io.png" alt="my publications on itch"></a>
+          <a href="https://podcasts.apple.com/au/podcast/play-worlds-podcast/id1722152993" title="Play Worlds Podcast"><img src="assets/images/social/podcast.png" alt="Play Worlds Podcast"></a>
+          <a href="https://www.youtube.com/channel/UCy0_3iGguFBabUOdQ2o_ZUQ" title="youtube"><img src="assets/images/social/youtube.png" alt="youtube"></a>
+          <a href="/assets/rss/blog.xml"><img src="assets/images/social/rss.png" title="blog rss" alt="blog rss"></a>
+        </div>
       </div>
     `
   }
