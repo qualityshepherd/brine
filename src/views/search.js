6d7309e631b4737519efbc7c9eb2b611c6312fb4
Brine <brian@qualityshepherd.com>
2024-03-24 07:16:16 -0600

    archive!

--- src/views/search.js
+++ src/views/search.js
@@ -13,7 +13,7 @@ const search = {
 
     const posts = found.map(post => {
       return `
-        <article class="post border">
+        <article class="post">
           <a href="#post?s=${post.meta.slug}"><h2 class="post-title">${post.meta.title}</h2></a>
           <div class="date">${post.meta.date}</div>
           <div>${post.html}</div>
