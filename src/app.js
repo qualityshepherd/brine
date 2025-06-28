9b91f59f260084054c85974f49c5903d679a409d
Brine <brian@qualityshepherd.com>
2025-06-28 15:27:59 -0600

    more fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import { readSiteIndex, state } from './state.js'
 import { elements } from './dom.js'
-import { renderPosts } from './ui.js'
 import { handleLoadMore, handleRouting, handleSearch, toggleMenu } from './handlers.js'
 import config from './config.js'
 
+// Event listeners
 elements.menu.addEventListener('click', toggleMenu)
 elements.searchInput.addEventListener('input', handleSearch)
 elements.loadMore?.addEventListener('click', handleLoadMore)
 window.addEventListener('hashchange', handleRouting)
 
-// initial render
 ;(async () => {
   const index = await readSiteIndex(config.pathToIndex)
   state.posts = index
+  state.displayedPosts = config.maxPosts
   handleRouting()
-  renderPosts(state.posts, state.displayedPosts)
 })()
