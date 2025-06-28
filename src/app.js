237cca6c1c79bec35791f1b7445ada63f43c2300
Brine <brian@qualityshepherd.com>
2025-06-28 15:16:39 -0600

    fix for deep links

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ elements.menu.addEventListener('click', toggleMenu)
 elements.searchInput.addEventListener('input', handleSearch)
 elements.loadMore?.addEventListener('click', handleLoadMore)
 window.addEventListener('hashchange', handleRouting)
-window.addEventListener('DOMContentLoaded', handleRouting)
 
 // initial render
 ;(async () => {
   const index = await readSiteIndex(config.pathToIndex)
   state.posts = index
-  renderPosts(state.posts, state.displayedPosts) // Changed from postLimit to displayedPosts
+  handleRouting()
+  renderPosts(state.posts, state.displayedPosts)
 })()
